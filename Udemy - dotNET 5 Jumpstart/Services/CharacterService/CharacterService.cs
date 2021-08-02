210ff32b10a0cc49f8ab147e38cfcd8b8e0d014b
Gourimath, Sagar <sagar.gourimath@gmail.com>
2021-08-02 18:19:17 +0530

    CRUD with entity framework.

--- Udemy - dotNET 5 Jumpstart/Services/CharacterService/CharacterService.cs
+++ Udemy - dotNET 5 Jumpstart/Services/CharacterService/CharacterService.cs
@@ -4,60 +4,66 @@ using System.Linq;
 using System.Threading.Tasks;
 using Udemy___dotNET_5_Jumpstart.Models;
 using Udemy___dotNET_5_Jumpstart.DTOs.Character;
+using Udemy___dotNET_5_Jumpstart.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Udemy___dotNET_5_Jumpstart.Services.CharacterService
 {
-  public class CharacterService : ICharacterService
-  {
-    private static List<Character> characters = new List<Character>
+    public class CharacterService : ICharacterService
     {
-        new Character(),
-        new Character { Id = 1, Name = "Sam" }
-    };
-    private readonly IMapper _mapper;
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
-    public CharacterService(IMapper mapper)
-    {
-      _mapper = mapper;
-    }
-    public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
-    {
-      var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-      var character = _mapper.Map<Character>(newCharacter);
-      character.Id = characters.Max(c => c.Id) + 1;
-      characters.Add(character);
+        public CharacterService(IMapper mapper, DataContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
 
-      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
-      return serviceResponse;
-    }
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+            var character = _mapper.Map<Character>(newCharacter);
 
-    public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
-    {
-      var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
-      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
-      return serviceResponse;
-    }
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
 
-    public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
-    {
-      var serviceResponse = new ServiceResponse<GetCharacterDTO>();
-      serviceResponse.Data = _mapper.Map<GetCharacterDTO>(characters.FirstOrDefault(x => x.Id == id));
+            serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
+            return serviceResponse;
+        }
 
-      return serviceResponse;
-    }
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters()
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+            var dbCharacters = await _context.Characters.ToListAsync();
 
-    public async Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updatedCharacter)
-    {
-      var serviceResponse = new ServiceResponse<GetCharacterDTO>();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDTO>();
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+
+            serviceResponse.Data = _mapper.Map<GetCharacterDTO>(dbCharacter);
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updatedCharacter)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDTO>();
             try
             {
-                Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+                Character character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
                 character.Name = updatedCharacter.Name;
                 character.HitPoints = updatedCharacter.HitPoints;
                 character.Strength = updatedCharacter.Strength;
                 character.Defence = updatedCharacter.Defence;
                 character.Intelligence = updatedCharacter.Intelligence;
                 character.Class = updatedCharacter.Class;
+
+                await _context.SaveChangesAsync();
 
                 serviceResponse.Data = _mapper.Map<GetCharacterDTO>(character);
             }
@@ -67,18 +73,19 @@ namespace Udemy___dotNET_5_Jumpstart.Services.CharacterService
                 serviceResponse.Message = ex.Message;
             }
 
-      return serviceResponse;
-    }
+            return serviceResponse;
+        }
 
-    public async Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int Id)
-    {
-      var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
+        public async Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacter(int Id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             try
             {
-                Character character = characters.First(c => c.Id == Id);
-                characters.Remove(character);
+                Character character = await _context.Characters.FirstAsync(c => c.Id == Id);
+                _context.Characters.Remove(character);
+                await _context.SaveChangesAsync();
 
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
+                serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToListAsync();
             }
             catch (System.Exception ex)
             {
@@ -86,7 +93,7 @@ namespace Udemy___dotNET_5_Jumpstart.Services.CharacterService
                 serviceResponse.Message = ex.Message;
             }
 
-      return serviceResponse;
+            return serviceResponse;
+        }
     }
-  }
 }
